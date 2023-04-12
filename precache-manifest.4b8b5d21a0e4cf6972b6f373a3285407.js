273f536e456bf2e2b3f4f8a40ce22b2d97937866
ChristianSanchez <chris.346@hotmail.com>
2023-04-12 11:57:49 -0400

    Updates

---
+++ precache-manifest.4b8b5d21a0e4cf6972b6f373a3285407.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0c8db3f65386aae1bf395962b77b76",
+    "url": "/phone-cart-react/index.html"
+  },
+  {
+    "revision": "fe55408506519d217346",
+    "url": "/phone-cart-react/static/css/main.8db4b5e8.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/phone-cart-react/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/phone-cart-react/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe55408506519d217346",
+    "url": "/phone-cart-react/static/js/main.199f7938.chunk.js"
+  },
+  {
+    "revision": "75b036489bbcb2b49928",
+    "url": "/phone-cart-react/static/js/runtime-main.a5bb3f27.js"
+  }
+]);
